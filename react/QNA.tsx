c865a5b5be0721b0f7364e5fe12145f6b6852b77
Tony Sheng <tony.sheng@vtex.com.br>
2020-12-15 15:59:50 -0500

    Update classNames to use the imported stylesheet

--- react/QNA.tsx
+++ react/QNA.tsx
@@ -7,61 +7,61 @@ const QuestionsAndAnswers: FC = () => {
     <div>
       <h2 className={styles['qna-header']}>Customer Questions &amp; Answers</h2>
 
-      <div className="qna-search-container">
+      <div className={styles['qna-search-container']}>
         <input
           type="search"
           name="qna-search"
-          className="qna-search"
+          className={styles['qna-search']}
           placeholder="Have a question? Search for answers"
         />
       </div>
 
-      <div className="votes-question-container">
-        <div className="votes">
-          <div className="button-container">
-            <button className="increment"></button>
+      <div className={styles['votes-question-container']}>
+        <div className={styles['votes']}>
+          <div className={styles['button-container']}>
+            <button className={styles['increment']}></button>
           </div>
-          <div className="vote-count">1</div>
-          <div className="vote-text">vote</div>
-          <div className="button-container">
-            <button className="decrement"></button>
+          <div className={styles['vote-count']}>1</div>
+          <div className={styles['vote-text']}>vote</div>
+          <div className={styles['button-container']}>
+            <button className={styles['decrement']}></button>
           </div>
         </div>
 
-        <div className="question-answer-container">
-          <div className="question-container">
-            <div className="question-label">Question:</div>
-            <a className="question-text">
+        <div className={styles['question-answer-container']}>
+          <div className={styles['question-container']}>
+            <div className={styles['question-label']}>Question:</div>
+            <a className={styles['question-text']}>
               Does the stand detach from the microphone body?
             </a>
           </div>
 
-          <div className="no-answer">No answers yet. Be the first!</div>
+          <div className={styles['no-answer']}>No answers yet. Be the first!</div>
         </div>
       </div>
 
-      <div className="votes-question-container">
-        <div className="votes">
-          <div className="button-container">
-            <button className="increment"></button>
+      <div className={styles['votes-question-container']}>
+        <div className={styles['votes']}>
+          <div className={styles['button-container']}>
+            <button className={styles['increment']}></button>
           </div>
-          <div className="vote-count">1</div>
-          <div className="vote-text">vote</div>
-          <div className="button-container">
-            <button className="decrement"></button>
+          <div className={styles['vote-count']}>1</div>
+          <div className={styles['vote-text']}>vote</div>
+          <div className={styles['button-container']}>
+            <button className={styles['decrement']}></button>
           </div>
         </div>
 
-        <div className="question-answer-container">
-          <div className="question-container">
-            <div className="question-label">Question:</div>
-            <a className="question-text">
+        <div className={styles['question-answer-container']}>
+          <div className={styles['question-container']}>
+            <div className={styles['question-label']}>Question:</div>
+            <a className={styles['question-text']}>
               Does the stand detach from the microphone body?
             </a>
           </div>
-          <div className="answer-container">
-            <div className="answer-label">Answer:</div>
-            <div className="answer-text">
+          <div className={styles['answer-container']}>
+            <div className={styles['answer-label']}>Answer:</div>
+            <div className={styles['answer-text']}>
               Thanks for your question! It is important that you store your
               brewer in a safe and frost free environment. If your brewer has
               been in an environment below freezing, let it warm to room
@@ -69,32 +69,32 @@ const QuestionsAndAnswers: FC = () => {
               helpful!
             </div>
           </div>
-          <div className="additional-info">By Chris on March 29, 2020</div>
+          <div className={styles['additional-info']}>By Chris on March 29, 2020</div>
         </div>
       </div>
 
-      <div className="votes-question-container">
-        <div className="votes">
-          <div className="button-container">
-            <button className="increment"></button>
+      <div className={styles['votes-question-container']}>
+        <div className={styles['votes']}>
+          <div className={styles['button-container']}>
+            <button className={styles['increment']}></button>
           </div>
-          <div className="vote-count">1</div>
-          <div className="vote-text">vote</div>
-          <div className="button-container">
-            <button className="decrement"></button>
+          <div className={styles['vote-count']}>1</div>
+          <div className={styles['vote-text']}>vote</div>
+          <div className={styles['button-container']}>
+            <button className={styles['decrement']}></button>
           </div>
         </div>
 
-        <div className="question-answer-container">
-          <div className="question-container">
-            <div className="question-label">Question:</div>
-            <a className="question-text">
+        <div className={styles['question-answer-container']}>
+          <div className={styles['question-container']}>
+            <div className={styles['question-label']}>Question:</div>
+            <a className={styles['question-text']}>
               Does the stand detach from the microphone body?
             </a>
           </div>
-          <div className="answer-container">
-            <div className="answer-label">Answer:</div>
-            <div className="answer-text">
+          <div className={styles['answer-container']}>
+            <div className={styles['answer-label']}>Answer:</div>
+            <div className={styles['answer-text']}>
               Thanks for your question! It is important that you store your
               brewer in a safe and frost free environment. If your brewer has
               been in an environment below freezing, let it warm to room
@@ -102,13 +102,13 @@ const QuestionsAndAnswers: FC = () => {
               helpful!
             </div>
           </div>
-          <div className="additional-info">By Chris on March 29, 2020</div>
-          <a className="questions-dropdown">See more answers (2)</a>
-          <button className="collapse-button">Collapse all answers</button>
+          <div className={styles['additional-info']}>By Chris on March 29, 2020</div>
+          <a className={styles['questions-dropdown']}>See more answers (2)</a>
+          <button className={styles['collapse-button']}>Collapse all answers</button>
         </div>
       </div>
 
-      <button className="more-questions-button">
+      <button className={styles['more-questions-button']}>
         See more answered questions (10)
       </button>
     </div>
