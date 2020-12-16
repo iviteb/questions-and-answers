c7e6fe39749c05b675af0327be4ca8257045ed46
Tony Sheng <tony.sheng@vtex.com.br>
2020-12-16 15:00:45 -0500

    Add modal skeleton

--- react/QNA.tsx
+++ react/QNA.tsx
@@ -1,20 +1,36 @@
 /* eslint-disable no-console */
-import React, { FC, useContext } from 'react'
+import React, { FC, useContext, useState } from 'react'
 import { compose, graphql } from 'react-apollo'
 import { injectIntl } from 'react-intl'
 
 import { ProductContext } from 'vtex.product-context'
+import { Button, ModalDialog } from 'vtex.styleguide'
 
 import QUERY_CONFIG from './queries/config.gql'
 
 import styles from './qnastyle.css'
 
+
 const QuestionsAndAnswers: FC<any> = ({ data: { config } }) => {
+  
+  const [state, setState] = useState<any>({
+    isModalOpen: false,
+  })
+  
+  const {isModalOpen} = state
+  
+  const handleModalToggle = () => {
+    setState({ ...state, isModalOpen: !isModalOpen })
+  }
+
   console.log('QuestionsAndAnswers =>', ProductContext)
   const { product } = useContext(ProductContext) as any
 
+  
   console.log('Config =>', config)
   console.log('Product =>', product)
+
+  
 
   if (!config) return null
 
@@ -136,6 +152,49 @@ const QuestionsAndAnswers: FC<any> = ({ data: { config } }) => {
       <button className={styles['more-questions-button']}>
         See more answered questions (10)
       </button>
+
+      <div className={styles['create-question-container']}>
+        <Button onClick={() => handleModalToggle()}>Open modal</Button>
+      </div>
+
+      <div className={styles['modal-container']}>
+
+        <ModalDialog
+          centered
+          isOpen={isModalOpen}
+          onClose={handleModalToggle}>
+          <div className="dark-gray">
+            <p>
+              The Payments module is the system responsible for managing all
+              actions regarding your store's cash flow.
+            </p>
+
+            <p>
+              Before we explore the features within VTEX Admin Payments, let's
+              clarify some important concepts regarding the payment flow of an
+              order. This process is performed by some actors within the
+              Brazilian financial system, which make up the Payments module
+              architecture.
+            </p>
+            <div
+              style={{
+                backgroundColor: '#edf4fa',
+                borderRadius: '4px',
+                border: 'solid #368df7',
+                borderWidth: '0 0 0 4px',
+                boxSizing: 'border-box',
+                padding: '12px 16px',
+              }}>
+              It is important to remember that each store has its own
+              particularities and its own operation, which influence how to
+              build your business' Payment module. To set up your cash flow, it
+              is therefore crucial to keep in mind the real needs and purposes
+              of the retailer and of the desired project.
+            </div>
+          </div>
+        </ModalDialog>
+      </div>
+
     </div>
   )
 }
