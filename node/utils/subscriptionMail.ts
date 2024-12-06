5fe94eb926f29c6234a54ff40cccce6c26ec0163
razvanudream <71461884+razvanudream@users.noreply.github.com>
2024-12-06 12:25:35 +0200

    Upgraded node builder to 7.x (#10)

--- node/utils/subscriptionMail.ts
+++ node/utils/subscriptionMail.ts
@@ -1,24 +1,16 @@
-import { SCHEMA_VERSION } from "../resolvers"
+import { SCHEMA_VERSION } from '../resolvers'
 
 const subscriptionMail = async (ctx: Context, questionId: string) => {
   const {
-    clients: {
-      masterdata,
-      mail,
-      apps,
-      catalogGraphQL
-    },
-    vtex: {
-      host,
-      production
-    }
+    clients: { masterdata, mail, apps, catalogGraphQL },
+    vtex: { host, production },
   } = ctx
 
   const { subscriptionEmailTemplate } = await apps.getAppSettings(
     `${process.env.VTEX_APP_ID}`
   )
 
-  if(subscriptionEmailTemplate){
+  if (subscriptionEmailTemplate) {
     const subscriptions: any = await masterdata.searchDocuments({
       dataEntity: 'subscriptions',
       schema: SCHEMA_VERSION,
@@ -26,15 +18,15 @@ const subscriptionMail = async (ctx: Context, questionId: string) => {
       where: `questionId=${questionId}`,
       pagination: {
         page: 1,
-        pageSize: 999
-      }
+        pageSize: 999,
+      },
     })
 
-    if(subscriptions.length) {
-      const { question, productId } = await masterdata.getDocument({
+    if (subscriptions.length) {
+      const { question, productId } = await masterdata.getDocument<any>({
         dataEntity: 'qna',
         id: questionId,
-        fields: ['question', 'productId']
+        fields: ['question', 'productId'],
       })
 
       const { product }: any = await catalogGraphQL.product(productId)
@@ -43,11 +35,16 @@ const subscriptionMail = async (ctx: Context, questionId: string) => {
         const options = {
           toEmail: subscription.email,
           questionText: question,
-          productUrl: production ? `https://pentruanimale.ro/${product.linkId}/p` : `https://${host}/${product.linkId}/p`,
-          unsubscribeUrl: production ? `https://pentruanimale.ro/qna/unsubscribe/${subscription.id}` : `https://${host}/qna/unsubscribe/${subscription.id}`,
+          productUrl: production
+            ? `https://pentruanimale.ro/${product.linkId}/p`
+            : `https://${host}/${product.linkId}/p`,
+          unsubscribeUrl: production
+            ? `https://pentruanimale.ro/qna/unsubscribe/${subscription.id}`
+            : `https://${host}/qna/unsubscribe/${subscription.id}`,
         }
+
         mail.send(options, subscriptionEmailTemplate)
-      });
+      })
     }
   }
 }
